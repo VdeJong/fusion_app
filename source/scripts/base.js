e0c37424194c10e8b0cb205bbbb7d467ba1b2a51
Brian Roos <brianroos94@gmail.com>
2017-03-29 14:16:33 +0200

    Chat pagina

--- source/scripts/base.js
+++ source/scripts/base.js
@@ -1,5 +1,30 @@
 function init() {
   // Init Foundation
   $(document).foundation();
+
+  // Character count message
+  counter = function() {
+    var value = $('#message').val();
+
+    if (value.length == 0) {
+      $('#totalChars').html(0);
+      return;
+    }
+
+    var regex = /\s+/gi;
+    var totalChars = value.length;
+    $('#totalChars').html(totalChars);
+  }
 }
 init();
+
+$(document).ready(function() {
+  if($('#message')) {
+    $('#message').change(counter);
+    $('#message').keydown(counter);
+    $('#message').keypress(counter);
+    $('#message').keyup(counter);
+    $('#message').blur(counter);
+    $('#message').focus(counter);
+  }
+});
